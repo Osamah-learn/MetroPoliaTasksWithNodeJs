67c52c3feb1885ecfe7eb58e57331ec4124c8385
Osamah-learn <osamah.ammer2@gmaill.com>
2022-02-24 19:17:47 +0200

    mkdir  views

--- Pug template engine/server.js
+++ Pug template engine/server.js
@@ -1,9 +1,36 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
-
 const port = 8080;
+app.set("view engine", "pug");
 
+let movies = [
+  {
+    id: "1588323375416",
+    title: "Star Wars: Episode IX - The Rise of Skywalker",
+    year: 2019,
+    director: "J.J. Abrams",
+  },
+  {
+    id: "1588323390624",
+    title: "The Irishman",
+    year: 2019,
+    director: "Martin Scorsese",
+  },
+  {
+    id: "1588323412643",
+    title: "Harry Potter and the Sorcerers Stone",
+    year: 2001,
+    director: "Chris Columbus",
+  },
+];
+
+app.get("/movies", (req, res) => {
+  res.render("movielist", { movies });
+});
+
+app.get("/hello", (req, res) => {
+  res.render("hello", { firstname: "Osamah", lastname: "Amer" });
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
